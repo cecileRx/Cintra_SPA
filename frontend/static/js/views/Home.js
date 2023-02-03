35d70671685f22e8ed2e5d8a4eccc65ca139294b
cecile rougnaux <cecile_rougnaux@hotmail.com>
2023-02-03 16:21:39 +0100

    audio player fixed bottom

--- frontend/static/js/views/Home.js
+++ frontend/static/js/views/Home.js
@@ -3131,14 +3131,14 @@ export default class extends AbstractView {
     </div>
       <div class="title">
       <div class="signature">
-          <h3>MIGUEL CINTRA</h3>
-          <p>composer . sound designer . music producer</p>
+          <h2>Miguel Cintra</h2>
+          <p>sound designer . composer . music producer</p>
       </div>
       <div class="contact">
-          <p class="contact-mail">miguelmcintra@gmail.com</p>
           <div class="social-media">
             <a href="https://www.linkedin.com/in/miguel-cintra/"> <i class="fa-brands fa-linkedin"></i></a>
             <a href="https://miguelcintra.bandcamp.com/"> <i class="fa-brands fa-bandcamp"></i></a>
+            <a href="https://miguelcintra.bandcamp.com/"> <i class="fa-brands fa-youtube"></i></a>
           </div>
       </div>
     </div>

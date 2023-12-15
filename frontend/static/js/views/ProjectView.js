5f2727f0dd9bb3348d391b0633972e8bdba7ee1f
cecile rougnaux <cecile_rougnaux@hotmail.com>
2023-12-15 15:21:38 +0000

    git ignore added

--- frontend/static/js/views/ProjectView.js
+++ frontend/static/js/views/ProjectView.js
@@ -17,21 +17,19 @@ export default class extends AbstractView {
 
     function filterById(jsonObject, id) { return jsonObject.filter(function (jsonObject) { return (jsonObject['id'] == id); })[0]; }
 
-    let media = filterById(mediaList, this.params.id);
+   
     console.log(media.url_link)
 
     return `
-
-      <div class="video-project">
-        <h2 class="video-project-title"> ${media.name} </h2>
-        <div class="video-container">
-          <iframe src="https://www.youtube.com/embed/XUi580gA5BQ" style="margin-right: 1%; margin-left: 1%;" width="98%" height=""  title=${media.url_title}
-        frameborder="0" allowfullscreen></iframe>
-         </div>
-        <p class="video-description">${media.url_description}</p>
-      </div>
+      <h1> ${media.name} </h1>
+      <p>
+         <iframe src=${media.url_src} width="560" height="315" title=${media.url_title}
+      frameborder="0" allowfullscreen></iframe>
+      </p>
     `;
   }
 }
+
+
 
 
